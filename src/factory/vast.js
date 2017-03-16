18290e8e3bf4e45e95523e40c20dbb2f06ad2afb
Pieter Delbeke <pieter.delbeke@zentrick.com>
2017-03-16 14:49:26 +0100

    fix: ad elements with a sequence belong to an ad-pod, others to the buffer

--- src/factory/vast.js
+++ src/factory/vast.js
@@ -1,32 +1,38 @@
 import {VAST, AdPod} from 'iab-vast-model'
 import createAd from './ad'
 
-const createPod = ($ads, options) => {
-  const pod = new AdPod()
-  for (const $ad of $ads) {
-    try {
-      const createdAd = createAd($ad, options)
-      pod.ads.add(createdAd)
-    } catch (err) {
-      options.errorHandler(err)
+const createPod = ($vast, vast, options) => {
+  const podAds = $vast.ad
+    .filter($ad => typeof $ad.sequence !== 'undefined')
+    .map($ad => createAd($ad, options))
+  if (podAds.length > 0) {
+    vast.adPod = new AdPod()
+    for (let i = 0; i < podAds.length; i++) {
+      vast.adPod.ads.add(podAds[i])
     }
   }
-  return pod
+}
+
+const createBuffet = ($vast, vast, options) => {
+  const buffetAds = $vast.ad
+    .filter($ad => typeof $ad.sequence === 'undefined')
+    .map($ad => createAd($ad, options))
+  if (buffetAds.length > 0) {
+    for (let i = 0; i < buffetAds.length; i++) {
+      vast.ads.add(buffetAds[i])
+    }
+  }
 }
 
 export default ($vast, options) => {
   const vast = new VAST()
   vast.version = $vast.version
   if ($vast.ad && $vast.ad.length > 0) {
-    if ($vast.ad.length > 1) {
-      vast.adPod = createPod($vast.ad, options)
-    } else {
-      try {
-        const createdAd = createAd($vast.ad[0], options)
-        vast.ads.add(createdAd)
-      } catch (err) {
-        options.errorHandler(err)
-      }
+    try {
+      createPod($vast, vast, options)
+      createBuffet($vast, vast, options)
+    } catch (err) {
+      options.errorHandler(err)
     }
   }
   return vast
