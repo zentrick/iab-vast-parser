abf01e3521f3f9e8e9583658e9f2438483ddb2b4
Tim De Pauw <timdp@users.noreply.github.com>
2019-12-16 10:19:07 +0100

    Handle empty <Duration> (#59)

--- src/util/parse-time.js
+++ src/util/parse-time.js
@@ -1,9 +1,12 @@
 const re = /^\s*(\d+):(\d+):(\d+(?:\.\d+)?)\s*$/
 
 export default (str) => {
+  if (typeof str !== 'string') {
+    return null
+  }
   const m = re.exec(str)
   if (m == null) {
-    throw new Error(`Failed to parse time "${str}"`)
+    return null
   }
   return ((parseInt(m[1], 10) * 60) + parseInt(m[2], 10)) * 60 + parseFloat(m[3])
 }
